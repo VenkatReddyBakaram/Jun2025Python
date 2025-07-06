edcbfa640e1e4b0b67cb0053b9939bc4c21ae51c
= <VenkatReddyBakaram@gmail.com>
2025-07-06 21:11:36 +0530

    first commit

---
+++ p1.py
@@ -0,0 +1,107 @@
+print("Demo on Arithmatic Operators")
+
+a=5
+b=2
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+
+print("Assingment Operator")
+
+b=250
+print(b)
+
+print("Relational Operators")
+
+x=100
+y=50
+print(x>y)
+print(100>5000)
+print(100<5)
+print(50==50)
+print(50!=50)
+print(50>=50)
+print(50<=50)
+print(50>50)
+
+print("Logical Opeators")
+print(10>5 and 10>3 and 7>5)
+print(10>5 and 10<5)
+print(10<5 or 30>15)
+print(10>50 or 50>55)
+
+
+print((4>2 and 4>3) or 55<6 and (4>2 and 4>3) or 55<6 and (4>2 and 4>3) or 55<6)
+
+
+ 
+print(not 5>6)
+print(not 5==5)
+
+
+print("Identity Operator")
+
+x = [100,200]
+y = [100,200]
+
+print(x is y)
+print(x is not y)
+
+z = x
+print(z is x)
+print(z is not x)
+
+
+print("Membership Operators")
+
+colours = ["red","pink","green"]
+
+print("red" in colours)
+print("yellow" in colours)
+
+print("yellow" not in colours)
+print("Pink" in colours)
+
+
+print("Shorthand Operators")
+
+a = 100
+b = 200
+
+
+a += b
+
+print(a)
+a *= b
+print(a)
+a -= b
+print(a)
+x = 100
+y = 200
+
+z= x+y
+x+=y
+
+
+print("Bitwise Operators")
+
+print(8&9)
+print(8|9)
+
+print(8^9)
+print(~8)
+
+print(8<<2)
+print(8<<3)
+print(8<<4)
+print(8>>2)
+
+
+
+
